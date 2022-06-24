9d9612e11eddfbcda6461d157a32df864425e6f1
Dalton Turner <turnerdalton055@gmail.com>
2022-06-23 18:29:00 -0700

    Updating site footer (#22)
    * Updating site footer
    
    * build failure
    
    * footer issues
    
    * Finalizing polish

--- theme.config.js
+++ theme.config.js
@@ -2,20 +2,24 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Dalton Turner.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
+    <div>
+      <hr />
+      <a href="https://twitter.com/dangerdalt" target="_blank">
+        Twitter
+      </a>{" "}
+      ·{" "}
+      <a href="https://github.com/daltonturner" target="_blank">
+        GitHub
+      </a>
+      <small style={{ display: "block", marginTop: "8rem" }}>
+        <time>{YEAR}</time> © Dalton Turner.
+        <a href="/feed.xml">RSS</a>
+        <style jsx>{`
+          a {
+            float: right;
           }
-        }
-      `}</style>
-    </small>
-  )
+        `}</style>
+      </small>
+    </div>
+  ),
 }
