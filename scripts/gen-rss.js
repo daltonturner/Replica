e7bec47ca0cba0a235b961209ca3fc72389e670b
Dalton Turner <turnerdalton055@gmail.com>
2022-07-05 21:10:02 -0700

    Update feed params

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Dalton Turner',
+    site_url: 'https://daltonturner.xyz',
+    feed_url: 'https://daltonturner.xyz/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
