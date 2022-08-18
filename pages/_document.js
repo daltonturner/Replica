88b30eff0d34b42aec272539755b2126889808f0
Dalton Turner <turnerdalton055@gmail.com>
2022-08-17 22:09:32 -0700

    Update _document.js (#49)
    Resolves #36

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Replica',
+    description: 'Thoughts, Projects, Ideas',
+    image: 'https://github.com/daltonturner/Replica/blob/main/public/images/blobbyThumbnail.png'
   }
 
   return (
